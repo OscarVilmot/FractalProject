0e5d5604d9fb745211fe4517f3027edc42d19afc
OscarLeSuperBGGigaFITQuiVaALaSalle <oscar.vilmot@gmail.com>
2022-04-05 17:15:59 -0700

    Finalisation Julia + Sliders (need improvement)

--- fractal/app.py
+++ fractal/app.py
@@ -1,22 +1,39 @@
-from fractal.common.GUI import showGUI
+from fractal.common.GUI import showGUI, sliders
 from fractal.common.JuliasFractal import calculateArray, fractalJulia
-from matplotlib.pyplot import subplots, subplots_adjust
+from matplotlib.pyplot import subplots_adjust, figure
 
 def run():
+    fig = figure(figsize=(6,7))
+    subplots_adjust(bottom=.3)
 
-    fig, ax = subplots()
-    subplots_adjust(bottom=0.35)
+    start = [-1.5,-1.5]
+    size = [3,3]
+    precision = 200
 
-    start = [-2,-2]
-    size = [4,4]
-    precision = 100
-
-    C1 = -0.038088
-    C2 = 0.9754633
+    C1 = 0
+    C2 = 0
 
     C = complex(C1, C2)
 
     A = fractalJulia(C)
     T = calculateArray(start, size, precision, A)
 
-    showGUI(fig, T)
+    sliderList = sliders(fig)
+    
+    def updateJulia(val):
+        c1 = sliderList[0].val
+        c2 = sliderList[1].val
+
+        C = complex(c1, c2)
+        A = fractalJulia(C)
+        T = calculateArray(start, size, precision, A)
+        showGUI(fig, T, sliderList)
+
+
+    sliderList[0].on_changed(updateJulia)
+    sliderList[1].on_changed(updateJulia)
+    
+    showGUI(fig, T, sliderList)
+
+
+
